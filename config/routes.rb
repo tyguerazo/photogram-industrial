8520f15e29e9c1c5029ec5c3d3f59cc22e60f9f1
ty <89826990+tyguerazo@users.noreply.github.com>
2025-05-22 07:32:58 +0000

    Added tabbed interface with liked photos

--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,11 @@ Rails.application.routes.draw do
   resources :photos
   resources :users, only: [:show]
 
-  get "/:username" => "users#show", as: :user
+  get ":username" => "users#show", as: :user
+  get ":username/liked" => "users#liked", as: :liked
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-end
+  # Render dynamic PWA files from app/views/pwa/* (reme
